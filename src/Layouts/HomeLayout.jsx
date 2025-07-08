24d95a941c1b49dd5b3c1d939ea46716575411d5
Sazid Ahamed Sifat <sazidahamed04@gmail.com>
2025-07-08 11:47:36 +0600

    Initilized

---
+++ src/Layouts/HomeLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeLayout = () => {
+    return (
+        <div>
+                jkhdjkhsgshgfjgh
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../Layouts/HomeLayout";
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        Component: HomeLayout
+    }
+])
+
+export default router;
